5f23cb105ca5e38ba33ee29cc603eabacdd3a112
wingsby <wingsby@163.com>
2018-11-02 15:40:09 +0800

    共享版

--- data/RF_test.py
+++ data/RF_test.py
@@ -13,30 +13,37 @@ from __future__ import print_function
 import tensorflow as tf
 from tensorflow.contrib.tensor_forest.python import tensor_forest
 from tensorflow.python.ops import resources
-
+import sys
+sys.path.append('/home/wingsby/develop/python/photovoltaicPower/')
 # Ignore all GPUs, tf random forest does not benefit from it.
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+from data.preprocess import getData, getValData
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
+
+# mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
+# X, Y = getData()
 
 # Parameters
-num_steps = 5000 # Total steps to train
-batch_size = 512 # The number of samples per batch
+num_steps = 10000
+batch_size = 64
 
-num_features = 784 # Each image is 28x28 pixels
-num_trees = 1000
+num_features = 11
+num_trees = 100
 max_nodes = 1000
 
 # Input and Target data
 X = tf.placeholder(tf.float32, shape=[None, num_features])
 # For random forest, labels must be integers (the class id)
-Y = tf.placeholder(tf.int32, shape=[None])
+Y = tf.placeholder(tf.float32, shape=[None])
 
 # Random Forest Parameters
 hparams = tensor_forest.ForestHParams(regression=True,
+                                      num_classes=1,
                                       num_features=num_features,
                                       num_trees=num_trees,
                                       max_nodes=max_nodes).fill()
@@ -49,29 +56,32 @@ loss_op = forest_graph.training_loss(X, Y)
 
 # Measure the accuracy
 infer_op, _, _ = forest_graph.inference_graph(X)
-correct_prediction = tf.equal(tf.argmax(infer_op, 1), tf.cast(Y, tf.int64))
-accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# correct_prediction = tf.equal(tf.argmax(infer_op, 1), tf.cast(Y, tf.int64))
+accuracy_op = tf.reduce_mean(tf.abs(infer_op-Y))
 
 # Initialize the variables (i.e. assign their default value) and forest resources
 init_vars = tf.group(tf.global_variables_initializer(),
-    resources.initialize_resources(resources.shared_resources()))
-
+                     resources.initialize_resources(resources.shared_resources()))
 # Start TensorFlow session
+val_x, val_y = getValData()
 sess = tf.Session()
-
 # Run the initializer
 sess.run(init_vars)
 
 # Training
-for i in range(1, num_steps + 1):
-    # Prepare Data
+i = 0
+for batch_x, batch_y in getData(batch_size):  # Prepare Data
     # Get the next batch of MNIST data (only images are needed, not labels)
-    batch_x, batch_y = mnist.train.next_batch(batch_size)
     _, l = sess.run([train_op, loss_op], feed_dict={X: batch_x, Y: batch_y})
     if i % 50 == 0 or i == 1:
         acc = sess.run(accuracy_op, feed_dict={X: batch_x, Y: batch_y})
         print('Step %i, Loss: %f, Acc: %f' % (i, l, acc))
+    i += 1
+    if i > num_steps:
+        break
 
 # Test Model
-test_x, test_y = mnist.test.images, mnist.test.labels
-print("Test Accuracy:", sess.run(accuracy_op, feed_dict={X: test_x, Y: test_y}))
+# test_x, test_y = mnist.test.images, mnist.test.labels
+forecast=sess.run([infer_op],feed_dict={X: val_x})
+print(forecast)
+print("Test Accuracy:", sess.run(accuracy_op, feed_dict={X: val_x, Y: val_y}))
--- data/preprocess.py
+++ data/preprocess.py
@@ -1,12 +1,110 @@
-with open("/dpdata/photovoltaic_power/data/train_1.csv") as file:
-    cnt = 0
-    titles = []
-    datalist = []
-    for line in file:
-        if cnt < 1:
-            titles = line.split(',')
-        else:
-            data = line.split(',')
-            datalist.append(data)
-        cnt += 1
-print(datalist.__len__())
+import time
+
+def getData(batch_size):
+    filename='/dpdata/photovoltaic_power/data/train_1.csv'
+    while (1):
+        with open(filename, encoding='utf-8') as file:
+            cnt = 0
+            titles = []
+            datalist = []
+            labels = []
+            for line in file:
+                if cnt < 1:
+                    titles = line.split(',')
+                else:
+                    data = line.split(',')
+                    ctime = time.strptime(data[0], "%Y-%m-%d %H:%M:%S")
+                    ndata = []
+                    # ndata.append(cnt)
+                    ndata.append(ctime[1])
+                    ndata.append(ctime[2])
+                    ndata.append(ctime[3])
+                    ndata.append(ctime[4])
+                    ndata.append(ctime[5])
+                    ndata.append(float(data[1]))
+                    ndata.append(float(data[2]))
+                    ndata.append(float(data[3]))
+                    ndata.append(float(data[4]))
+                    ndata.append(float(data[5]))
+                    ndata.append(float(data[6]))
+                    label = float(data[8])
+                    labels.append(label)
+                    datalist.append(ndata)
+                    if (datalist.__len__() >= batch_size):
+                        yield (datalist, labels)
+                        datalist = []
+                        labels = []
+                cnt += 1
+                # return datalist,labels
+                # print(datalist.__len__())
+
+
+def getValData():
+    filename = "/dpdata/photovoltaic_power/data/train_1.csv"
+    with open(filename, encoding='utf-8') as file:
+        cnt = 0
+        titles = []
+        datalist = []
+        labels = []
+        for line in file:
+            cnt += 1
+            if cnt < 60000:
+                continue
+            if cnt < 1:
+                titles = line.split(',')
+            else:
+                data = line.split(',')
+                ctime = time.strptime(data[0], "%Y-%m-%d %H:%M:%S")
+                ndata = []
+                # ndata.append(cnt)
+                ndata.append(ctime[1])
+                ndata.append(ctime[2])
+                ndata.append(ctime[3])
+                ndata.append(ctime[4])
+                ndata.append(ctime[5])
+                ndata.append(float(data[1]))
+                ndata.append(float(data[2]))
+                ndata.append(float(data[3]))
+                ndata.append(float(data[4]))
+                ndata.append(float(data[5]))
+                ndata.append(float(data[6]))
+                label = float(data[8])
+                labels.append(label)
+                datalist.append(ndata)
+
+    return datalist, labels
+
+def getPredictData():
+    filename = "/dpdata/photovoltaic_power/data/test_1.csv"
+    with open(filename, encoding='utf-8') as file:
+        cnt = 0
+        titles = []
+        datalist = []
+        labels = []
+        for line in file:
+            cnt += 1
+            if cnt < 60000:
+                continue
+            if cnt < 1:
+                titles = line.split(',')
+            else:
+                data = line.split(',')
+                ctime = time.strptime(data[0], "%Y-%m-%d %H:%M:%S")
+                ndata = []
+                # ndata.append(cnt)
+                ndata.append(ctime[1])
+                ndata.append(ctime[2])
+                ndata.append(ctime[3])
+                ndata.append(ctime[4])
+                ndata.append(ctime[5])
+                ndata.append(float(data[1]))
+                ndata.append(float(data[2]))
+                ndata.append(float(data[3]))
+                ndata.append(float(data[4]))
+                ndata.append(float(data[5]))
+                ndata.append(float(data[6]))
+                label = float(data[8])
+                labels.append(label)
+                datalist.append(ndata)
+# getData()
+# getValData()
